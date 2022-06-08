3b55a17545892fc29d221be50ff3e4831ad2dd46
root <root@DESKTOP-5026TIB.localdomain>
2022-06-08 10:08:09 +0700

    remove unnessary code

--- docs/_next/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/profiles/[userId]":["static\u002Fchunks\u002Fpages\u002Fprofiles\u002F[userId].js"],sortedPages:["\u002F_app","\u002F_error","\u002Fprofiles\u002F[userId]"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
